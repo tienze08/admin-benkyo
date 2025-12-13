f747d04820d68992913f05e591ef5c56c843fb4a
tienhuuvo <tienvh08082003@gmail.com>
2025-12-14 00:31:59 +0700

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,6 @@ const API_URL = "https://benkyo.live/";
 
 export const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
